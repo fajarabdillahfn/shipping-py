209d1b7282928269c5e1b0b83c310ffd6b43595b
fajar-abdillahfn <abdillah.fajar@tokopedia.com>
2023-06-02 22:19:17 +0700

    feat: add test

---
+++ main.py
@@ -0,0 +1,14 @@
+import doctest
+
+
+def min_cost(n, h):
+    """Find min cost of delivery
+
+    >>> min_cost(4, [1, 4, 2, 4])
+    [1, 2, 2, 3]
+    >>> min_cost(5, [100, 100, 100, 100, 1])
+    [5, 4, 3, 2, 1]
+    """
+
+
+doctest.testmod()
